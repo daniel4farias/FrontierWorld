a2d39d98a70efa6c26bfef725f5731b217a4eb71
Daniel Farias <daniel4farias@gmail.com>
2019-10-07 16:13:15 -0700

    Frontier Map

---
+++ overviewerConfig.js
@@ -0,0 +1,96 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "icons/marker_home.png",
+            "spawnMarker2x": "icons/marker_home_2x.png",
+            "queryMarker": "icons/marker_location.png",
+            "queryMarker2x": "icons/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "FrontierWorld",
+        "FrontierWorld - nether"
+    ],
+    "map": {
+        "debug": true,
+        "cacheTag": "1570414719",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Overworld Render of My World",
+            "zoomLevels": 11,
+            "defaultZoom": 1,
+            "maxZoom": 11,
+            "path": "FrontierOverworld",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "FrontierWorld",
+            "last_rendertime": 1570413596,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                2,
+                93,
+                2
+            ],
+            "minZoom": 0,
+            "spawn": [
+                2,
+                93,
+                2
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Overworld Render of Nether",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "FrontierNetherSplit",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "FrontierWorld - nether",
+            "last_rendertime": 1570413344,
+            "imgextension": "png",
+            "isOverlay": true,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                2,
+                93,
+                2
+            ],
+            "minZoom": 0,
+            "tilesets": [],
+            "spawn": false,
+            "north_direction": 0
+        }
+    ]
+};
